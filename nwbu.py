8ba50958ba84eecafce518d444bda0e3f85ccf69
burnout backup <burnout@techniek-linux.ccdefactorij.be>
2020-09-25 18:18:30 +0200

    more commands for dell

--- nwbu.py
+++ nwbu.py
@@ -14,9 +14,15 @@ def dellbu(host):
     c = pexpect.spawn(r'ssh {}'.format(host), encoding='utf-8')
     c.logfile = sys.stdout
     c.sendline('enable')
+    c.expect(r'sw1#')
     c.sendline(r'copy running-config scp://burnout@burnout/running-config')
     c.expect(r'Password:')
+    c.sendline(r'=niet voor scm=')
+    c.expect(r'(y/n)')
+    c.send(r'y')
+    c.expect(r'sw1#', timeout=120)
     c.sendline(r'exit')
+    c.expect(r'sw1>')
     c.sendline(r'exit')
     c.close()
 
