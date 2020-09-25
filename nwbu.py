82036586e899d14bd21389e65c8cd3adf14bd33d
burnout backup <burnout@techniek-linux.ccdefactorij.be>
2020-09-25 18:14:07 +0200

    first commands to dell

--- nwbu.py
+++ nwbu.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 from datetime import datetime
 from csv import reader
 import pexpect
@@ -10,7 +11,17 @@ isodatetime = datetime.now().isoformat()
 def dellbu(host):
     ''' backup for a dell unit
     '''
+    c = pexpect.spawn(r'ssh {}'.format(host), encoding='utf-8')
+    c.logfile = sys.stdout
+    c.sendline('enable')
+    c.sendline(r'copy running-config scp://burnout@burnout/running-config')
+    c.expect(r'Password:')
+    c.sendline(r'exit')
+    c.sendline(r'exit')
+    c.close()
 
 def fortigatebu(host):
     ''' backup for a fortigate unit
     '''
+
+dellbu('sw1')
